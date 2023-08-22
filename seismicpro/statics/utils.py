3ca5a388d83d4ec6189b4ae8c13e8c61f262d4fe
Alexander Kuvaev <alexandrkuvaev@yandex.ru>
2023-08-22 16:53:33 +0300

    Add utils for source/receiver headers extraction

--- seismicpro/statics/utils.py
+++ seismicpro/statics/utils.py
@@ -1,3 +1,10 @@
+import warnings
+
+import polars as pl
+
+from ..utils import to_list
+
+
 def get_uphole_correction_method(survey, uphole_correction_method):
     if uphole_correction_method not in {"auto", "time", "depth", None}:
         raise ValueError
@@ -12,3 +19,35 @@ def get_uphole_correction_method(survey, uphole_correction_method):
     if uphole_correction_method == "depth" and "SourceDepth" not in survey.available_headers:
         raise ValueError
     return uphole_correction_method
+
+
+def group_source_headers(survey, index_cols):
+    index_cols = to_list(index_cols)
+    all_cols_set = set(index_cols + ["SourceX", "SourceY", "SourceSurfaceElevation"])
+    if "SourceDepth" in survey.available_headers:
+        all_cols_set.add("SourceDepth")
+    if "SourceUpholeTime" in survey.available_headers:
+        all_cols_set.add("SourceUpholeTime")
+    all_cols = list(all_cols_set)
+    non_index_cols = list(all_cols_set - set(index_cols))
+
+    headers = pl.from_pandas(survey.get_headers(all_cols), rechunk=False)
+    duplicated_expr = pl.any_horizontal([pl.n_unique(col) > 1 for col in non_index_cols]).alias("HasDuplicatedHeaders")
+    headers = headers.groupby(index_cols).agg(pl.mean(non_index_cols), duplicated_expr).to_pandas()
+    if headers["HasDuplicatedHeaders"].any():
+        warnings.warn("Some sources have non-unique locations or uphole data. Statics may be inaccurate.")
+    return headers
+
+
+def group_receiver_headers(survey, index_cols):
+    index_cols = to_list(index_cols)
+    all_cols_set = set(index_cols + ["GroupX", "GroupY", "ReceiverGroupElevation"])
+    all_cols = list(all_cols_set)
+    non_index_cols = list(all_cols_set - set(index_cols))
+
+    headers = pl.from_pandas(survey.get_headers(all_cols), rechunk=False)
+    duplicated_expr = pl.any_horizontal([pl.n_unique(col) > 1 for col in non_index_cols]).alias("HasDuplicatedHeaders")
+    headers = headers.groupby(index_cols).agg(pl.mean(non_index_cols), duplicated_expr).to_pandas()
+    if headers["HasDuplicatedHeaders"].any():
+        warnings.warn("Some receivers have non-unique locations. Statics may be inaccurate.")
+    return headers
