06ed17edc3651dfe0ff0f7168c3d8e2021ae0dcf
Alexander Kuvaev <alexandrkuvaev@yandex.ru>
2023-08-24 13:51:04 +0300

    Add properties to tomo model grid

--- seismicpro/statics/tomo_model/grid.py
+++ seismicpro/statics/tomo_model/grid.py
@@ -4,14 +4,18 @@ import numpy as np
 import polars as pl
 
 from .dataset import TomoModelTravelTimeDataset
-from ...const import HDR_FIRST_BREAK
+from ..grid import Grid
 from ...utils import to_list, IDWInterpolator
 
 
-class Grid3D:
+class Grid3D(Grid):
+    dataset_class = TomoModelTravelTimeDataset
+
     def __init__(self, origin, shape, cell_size, survey=None):
-        origin = np.broadcast_to(origin, 3)
-        cell_size = np.broadcast_to(cell_size, 3)
+        super().__init__(survey)
+
+        origin = np.broadcast_to(origin, 3).astype(np.float64)
+        cell_size = np.broadcast_to(cell_size, 3).astype(np.float64)
         if (cell_size <= 0).any():
             raise ValueError
         shape = np.broadcast_to(shape, 3).astype(np.int32, casting="same_kind")
@@ -21,21 +25,61 @@ class Grid3D:
         self.origin = origin
         self.cell_size = cell_size
         self.shape = shape
-        self.survey = survey
+
+        self.z_cell_bounds = self.z_origin + self.z_cell_size * np.arange(self.n_z_cells + 1)
+        self.x_cell_bounds = self.x_origin + self.x_cell_size * np.arange(self.n_x_cells + 1)
+        self.y_cell_bounds = self.y_origin + self.y_cell_size * np.arange(self.n_y_cells + 1)
+
+        self.z_cell_centers = self.z_cell_size / 2 + self.z_cell_bounds[:-1]
+        self.x_cell_centers = self.x_cell_size / 2 + self.x_cell_bounds[:-1]
+        self.y_cell_centers = self.y_cell_size / 2 + self.y_cell_bounds[:-1]
+
         self.air_mask = None
-        if survey is not None:
+        if self.has_survey:
             # TODO: add surface elevation interpolator
             self._init_air_mask()
 
     @property
-    def has_survey(self):
-        return self.survey is not None
+    def z_origin(self):
+        return self.origin[0]
+
+    @property
+    def x_origin(self):
+        return self.origin[1]
+
+    @property
+    def y_origin(self):
+        return self.origin[2]
+
+    @property
+    def z_cell_size(self):
+        return self.cell_size[0]
+
+    @property
+    def x_cell_size(self):
+        return self.cell_size[1]
+
+    @property
+    def y_cell_size(self):
+        return self.cell_size[2]
+
+    @property
+    def n_z_cells(self):
+        return self.shape[0]
+
+    @property
+    def n_x_cells(self):
+        return self.shape[1]
+
+    @property
+    def n_y_cells(self):
+        return self.shape[2]
+
+    @property
+    def n_cells(self):
+        return self.n_z_cells * self.n_x_cells * self.n_y_cells
 
     def _init_air_mask(self):
-        z_min, x_min, y_min = self.origin
-        nz, nx, ny = self.shape
-        dz, dx, dy = self.cell_size
-
         headers = pl.concat([sur.get_polars_headers().lazy() for sur in to_list(self.survey)], rechunk=False)
         source_elevations = headers.select([
             pl.col("SourceX").alias("X"),
@@ -48,16 +92,18 @@ class Grid3D:
             pl.col("ReceiverGroupElevation").alias("Elevation")
         ])
         elevations = pl.concat([source_elevations, receiver_elevations], rechunk=False).select([
-            ((pl.col("X") - x_min) / dx).floor().cast(pl.Int32).alias("BinX"),
-            ((pl.col("Y") - y_min) / dy).floor().cast(pl.Int32).alias("BinY"),
+            ((pl.col("X") - self.x_origin) / self.x_cell_size).floor().cast(pl.Int32).alias("BinX"),
+            ((pl.col("Y") - self.y_origin) / self.y_cell_size).floor().cast(pl.Int32).alias("BinY"),
             pl.col("Elevation")
         ]).groupby("BinX", "BinY").agg(pl.max("Elevation"))
         max_elevations = elevations.collect().to_numpy()
-        max_elevation_interp = IDWInterpolator(max_elevations[:, :2], max_elevations[:, 2], neighbors=8)
-        bin_coords = np.array(np.meshgrid(np.arange(nx), np.arange(ny))).T.reshape(-1, 2)
-        max_elevation_grid = max_elevation_interp(bin_coords).reshape(nx, ny)
-        z_range = z_min + dz * np.arange(nz)
-        self.air_mask = z_range.reshape(-1, 1, 1) >= max_elevation_grid
+
+        used_cell_coords = self.origin[1:] + self.cell_size[1:] / 2 + self.cell_size[1:] * max_elevations[:, :2]
+        max_elevation_interp = IDWInterpolator(used_cell_coords, max_elevations[:, 2], neighbors=8)
+
+        cell_coords = np.array(np.meshgrid(self.x_cell_centers, self.y_cell_centers)).T.reshape(-1, 2)
+        max_elevation_grid = max_elevation_interp(cell_coords).reshape(self.n_x_cells, self.n_y_cells)
+        self.air_mask = self.z_cell_bounds[:-1, None, None] >= max_elevation_grid
 
     # IO
 
@@ -66,27 +112,21 @@ class Grid3D:
         survey_list = to_list(survey)
         dz, dx, dy = np.broadcast_to(cell_size, 3)
 
-        z_max = max(sur[["SourceSurfaceElevation", "ReceiverGroupElevation"]].max() for sur in survey_list)
+        z_max = max(max(sur["SourceSurfaceElevation"].max(), sur["ReceiverGroupElevation"].max())
+                    for sur in survey_list)
         if z_min >= z_max:
             raise ValueError
         nz = math.ceil((z_max - z_min) / dz)
 
-        coords = np.concatenate([survey[["SourceX", "SourceY"]], survey[["GroupX", "GroupY"]]], axis=0)
-        x_min, y_min = np.floor(coords.min(axis=0)).astype(np.int32) - spatial_margin * np.array([dx, dy])
-        x_max, y_max = np.ceil(coords.max(axis=0)).astype(np.int32) + spatial_margin * np.array([dx, dy])
+        x_min = min(min(sur["SourceX"].min(), sur["GroupX"].min()) for sur in survey_list) - spatial_margin * dx
+        x_max = max(max(sur["SourceX"].max(), sur["GroupX"].max()) for sur in survey_list) + spatial_margin * dx
         nx = math.ceil((x_max - x_min) / dx)
+
+        y_min = min(min(sur["SourceY"].min(), sur["GroupY"].min()) for sur in survey_list) - spatial_margin * dy
+        y_max = max(max(sur["SourceY"].max(), sur["GroupY"].max()) for sur in survey_list) + spatial_margin * dy
         ny = math.ceil((y_max - y_min) / dy)
 
         origin = (z_min, x_min, y_min)
         shape = (nz, nx, ny)
         cell_size = (dz, dx, dy)
         return cls(origin, shape, cell_size, survey=survey)
-
-    # Dataset generation
-
-    def create_dataset(self, survey=None, first_breaks_header=HDR_FIRST_BREAK, uphole_correction_method="auto"):
-        if survey is None:
-            if not self.has_survey:
-                raise ValueError("A survey to create a dataset must be passed")
-            survey = self.survey
-        return TomoModelTravelTimeDataset(survey, self, first_breaks_header, uphole_correction_method)
