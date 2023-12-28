532be3481416bc69c37b6e4119f57c757c36dc9c
nikita-klsh <nikita_007_94@mail.ru>
2023-12-28 12:05:03 +0000

    Add spectrum.py docs

--- seismicpro/spectrum.py
+++ seismicpro/spectrum.py
@@ -1,4 +1,4 @@
-"""Base class for various transforms of seismic wavefield. """
+"""Base class for the representation of seismic gather in different domains. """
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
@@ -9,7 +9,31 @@ from .decorators import batch_method, plotter
 
 
 class Spectrum:
-    """Base class for the representation of seismic gather in different domains. """
+    """Base class for various transforms of seismic wavefield. 
+    Implements general processing and visualization logic.
+
+    Parameters
+    ----------
+    spectrum : 2d np.ndarray
+        Spectrum values.
+    x_values : 1d np.array
+        Unit values for spectrum x axis.
+    y_values : 1d np.array
+        Unit values for spectrum y axis.
+    coords : Coordinates or None, optional, defaults to None
+        Spatial coordinates of the spectrum.
+
+    Attributes
+    ----------
+    spectrum : 2d np.ndarray
+        Spectrum values.
+    x_values : 1d np.array
+        Unit values for spectrum x axis.
+    y_values : 1d np.array
+        Unit values for spectrum y axis.
+    coords : Coordinates or None
+        Spatial coordinates of the spectrum.
+    """
     def __init__(self, spectrum, x_values, y_values, coords=None):
         self.spectrum = spectrum
         self.x_values = x_values
@@ -19,6 +43,7 @@ class Spectrum:
 
     @property
     def sample_interval(self):
+    """Sample interval of spectrum y_values. None if the axis is not uniform. """
         dy = np.diff(self.y_values)
         if np.allclose(dy, dy[0]):
             return dy[0]
@@ -44,6 +69,7 @@ class Spectrum:
 
     @batch_method(target="t", copy_src=False)
     def scale_norm(self):
+        """ Scale the spectrum along the y axis. """
         l2_norm = np.nansum(self.spectrum ** 2, axis=1, keepdims=True) ** 0.5
         self.spectrum = np.where(spectrum_max != 0, self.spectrum / l2_norm, 0)
         return self
@@ -52,6 +78,42 @@ class Spectrum:
     @plotter(figsize=(10, 9))
     def plot(self, vfunc=None, align_vfunc=True, grid=False, colorbar=True, x_label=None, x_ticker=None, y_label=None, y_ticker=None,
              title=None, clip_threshold_quantile=0.99, n_levels=10, ax=None,  interpolation=None, **kwargs):
+        """Plot spectrum and, optionally, vfuncs on on it.
+
+        Parameters
+        ----------
+        vfunc: VFUNC, iterable of VFUNC, optional, defaults to None
+            VFUNCs to be plotted on the spectrum.
+        align_vfunc: bool, optional, defaults to True
+            Whether aligh (cut or extend) VFUNC y_axis to spectrum y_axis.
+        grid : bool, optional, defaults to False
+            Specifies whether to draw a grid on the plot.
+        colorbar : bool or dict, optional, defaults to True
+            Whether to add a colorbar to the right of the velocity spectrum plot.
+            If `dict`, defines extra keyword arguments for `matplotlib.figure.Figure.colorbar`.
+        x_label : str, optional, defaults to None
+            The title of the x-axis.
+        x_ticklabels : list of str, optional, defaults to None
+            An array of labels for the x-axis.
+        x_ticker : dict, optional, defaults to None
+            Parameters for ticks and ticklabels formatting for the x-axis; see `.utils.set_ticks` for more details.
+        y_ticklabels : list of str, optional, defaults to None
+            An array of labels for the y-axis.            
+        title : str, optional, defaults to None
+            Plot title.
+        clip_threshold_quantile : float, optional, defaults to 0.99
+            Clip the velocity spectrum values by given quantile.
+        n_levels : int, optional, defaults to 10
+            The number of levels on the colorbar.
+        ax : matplotlib.axes.Axes, optional, defaults to None
+            Axes of the figure to plot on.
+        interpolation: str, optional, defaults to None
+            Interpolation method either `ax.imshow` for case uniform spectrum
+            or `NonUniformImage` in case non-uniform spectrum.
+        kwargs : misc, optional
+            Additional common keyword arguments for `x_ticker` and `y_tickers`.
+        """
+
         # Cast text-related parameters to dicts and add text formatting parameters from kwargs to each of them
         (title, x_ticker, y_ticker), kwargs = set_text_formatting(title, x_ticker, y_ticker, **kwargs)
 
